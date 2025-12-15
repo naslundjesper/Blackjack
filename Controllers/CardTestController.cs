a187c181a7bea71ad610b7ab0109d973a923bd11
naslundjesper <naslundjesper@icloud.com>
2025-12-15 14:56:07 +0100

    +Services mapp samt CardService och test

---
+++ Controllers/CardTestController.cs
@@ -0,0 +1,29 @@
+﻿using Blackjack.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blackjack.Controllers
+{
+    [ApiController]
+    [Route("api/test/cards")]
+    public class CardTestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            var cardService = new CardService();
+
+            var deck = cardService.CreateDeck();
+            cardService.Shuffle(deck, seed: 123);
+
+            int card1 = cardService.DrawCard(deck);
+            int card2 = cardService.DrawCard(deck);
+
+            return Ok(new
+            {
+                FirstCard = card1,
+                SecondCard = card2,
+                RemainingCards = deck.Count
+            });
+        }
+    }
+}
---
+++ Services/CardService.cs
@@ -0,0 +1,35 @@
+﻿namespace Blackjack.Services
+{
+    public class CardService
+    {
+        public List<int> CreateDeck()
+        {
+            var deck = new List<int>();
+            for (int i = 0; i < 52; i++)
+                deck.Add(i);
+
+            return deck;
+        }
+
+        public void Shuffle(List<int> deck, int seed)
+        {
+            var rng = new Random(seed);
+
+            for (int i = deck.Count - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                (deck[i], deck[j]) = (deck[j], deck[i]);
+            }
+        }
+
+        public int DrawCard(List<int> deck)
+        {
+            if (deck.Count == 0)
+                throw new InvalidOperationException("Deck is empty");
+
+            int card = deck[0];
+            deck.RemoveAt(0);
+            return card;
+        }
+    }
+}
