ad2e74ceb291b05820136e1dc978f53ae9612b1c
naslundjesper <naslundjesper@icloud.com>
2025-12-15 13:48:02 +0100

    EF Core tillagt, byt till .NET 8.xx så att allt fungerar som det ska

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -1,8 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Blackjack.Models;
 
 namespace Blackjack.Data
 {
-    public class DbContext
+    public class BlackjackDbContext : DbContext
     {
+        public BlackjackDbContext(DbContextOptions<BlackjackDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Round> Rounds { get; set; }
+        public DbSet<PlayerHand> PlayerHands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Player -> Game (Player1)
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.Player1)
+                .WithMany(p => p.GamesAsPlayer1)
+                .HasForeignKey(g => g.Player1ID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Player -> Game (Player2)
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.Player2)
+                .WithMany(p => p.GamesAsPlayer2)
+                .HasForeignKey(g => g.Player2ID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Blackjack.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Registrera EF Core + Azure SQL
+builder.Services.AddDbContext<BlackjackDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 var app = builder.Build();
 
